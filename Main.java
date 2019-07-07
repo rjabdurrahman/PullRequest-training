b735249e9b857ee05cd6702140bb138671e1af94
Codejune <kbj9704@gmail.com>
2019-07-07 23:42:34 +0600

    Organized
    Signed-off-by: Codejune <kbj9704@gmail.com>

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("My Name is KimByoungJun");
-		System.out.println("My name is Daeho");
+		System.out.println("My name is Danny");
 		System.out.println ("My name is Hafijur");
 		System.out.println ("My name is Alamin"); 
 		System.out.println ("My name is Washim Akram");
